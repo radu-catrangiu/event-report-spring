5109220d30fc1b0730e775f103581930ff91faa7
Radu Ovidiu CATRANGIU <rcatrangiu@bitdefender.com>
2020-05-17 02:37:51 +0300

    Clear import.

--- backend/src/main/java/com/dai/eventreport/imagesHandler/ImagesRepository.java
+++ backend/src/main/java/com/dai/eventreport/imagesHandler/ImagesRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
